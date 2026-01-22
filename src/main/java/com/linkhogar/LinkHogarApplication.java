92380d42f4baaa4149a170c321ed2b3c1d5960c2
Sergio Núñez <sergionunezsierra1@gmail.com>
2026-01-22 19:23:15 +0100

    Configuración inicial del proyecto

---
+++ src/main/java/com/linkhogar/LinkHogarApplication.java
@@ -0,0 +1,13 @@
+package com.linkhogar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkHogarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkHogarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkhogar/LinkHogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkhogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkHogarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
