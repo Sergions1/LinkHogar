fd1121878bcf89a8d62d5de66641fad31c545021
Sergions1 <sergionunezsierra1@gmail.com>
2026-03-19 09:43:17 +0100

    nuevo carrousel

--- linkhogar-web/src/app/pages/house/detail/detail.ts
+++ linkhogar-web/src/app/pages/house/detail/detail.ts
@@ -2,50 +2,66 @@ import {Component, inject, Input, OnInit, signal} from '@angular/core';
 import {HouseResponse} from '../../../Models/Houses/HouseResponse';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HouseService} from '../../../services/house/house-service';
-import {DecimalPipe} from '@angular/common';
+import {CommonModule, DecimalPipe} from '@angular/common';
 import {HouseForm} from '../create/create';
 
 @Component({
   selector: 'app-detail',
-  imports: [
-    DecimalPipe
-  ],
+  standalone: true,
+  imports: [DecimalPipe, CommonModule],
   templateUrl: './detail.html',
   styleUrl: './detail.scss',
 })
-export class Detail implements OnInit{
+export class Detail implements OnInit {
   private route = inject(ActivatedRoute);
   private houseService = inject(HouseService);
   private router = inject(Router);
 
- house= signal<HouseResponse | null>(null);
- isLoading = signal(false);
+  house = signal<HouseResponse | null>(null);
+  isLoading = signal(false);
+  houseId: string | null = null;
+  currentImageIndex = 0; // 👈
 
- houseId : string | null = null;
-
- @Input() previewData : HouseForm | null = null;
+  @Input() previewData: HouseForm | null = null;
 
   ngOnInit() {
-    if(this.previewData) return;
+    if (this.previewData) return;
     this.loadHouse();
+  }
+
+  prevImage(images: string[]) {
+    this.currentImageIndex = this.currentImageIndex === 0
+      ? images.length - 1
+      : this.currentImageIndex - 1;
+  }
+
+  nextImage(images: string[]) {
+    this.currentImageIndex = this.currentImageIndex === images.length - 1
+      ? 0
+      : this.currentImageIndex + 1;
+  }
+
+  goToImage(index: number) {
+    this.currentImageIndex = index;
   }
 
   loadHouse() {
     this.isLoading.set(true);
     this.route.paramMap.subscribe(param => {
       this.houseId = param.get('id');
-    })
-    if(this.houseId != null){
+    });
+    if (this.houseId != null) {
       this.houseService.getHouseById(this.houseId).subscribe({
         next: params => {
           this.house.set(params);
+          this.currentImageIndex = 0; // 👈 reset al cargar
           this.isLoading.set(false);
         },
         error: err => {
           console.log(err);
           this.isLoading.set(false);
         }
-      })
+      });
     }
   }
 }
